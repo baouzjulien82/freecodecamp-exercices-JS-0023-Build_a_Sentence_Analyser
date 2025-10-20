f5bea98ad4e9a916c7531b7faa76715a1b94cb75
Julien Baouz <baouzjulien@gmail.com>
2025-10-20 21:15:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,38 @@ const getVowelCount = (sentence) => {
 
   return count;
 };
+
+// *********************** Seconde partie, compter les consonnes d'une phrase ***********************
+// Version initiale
+
+function getConsonantCount(sentence) {
+  const vowels = "aeiou";
+  let count2 = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (!vowels.includes(char) && char >= 'a' && char <= 'z') { // char >=a et <= z pour ignorer les espaces et autre types de charactères
+      count2++;
+    }
+  }
+  return count2;
+}
+
+const consoneCount = getConsonantCount("Coding is fun");
+console.log(`Consone Count: ${consoneCount}`);
+
+// *********************** Troisième partie, compter la ponctuation ***********************
+// Version initiale
+
+const getPunctuationCount = (sentence) => {
+  let punctuationCount = 0;
+  for (const char of sentence) {
+    if(!/[a-zA-Z ]/.test(char)) {
+      punctuationCount++;
+    }
+  }
+  return punctuationCount;
+};
+
+// *********************** Quatrième partie, compter les mots dans une phrase ***********************
+// Version initiale
+
